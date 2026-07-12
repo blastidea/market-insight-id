ab4930e83a144e3e5ccd68224436d8fd6c71ad6b
blastidea <blastidea@gmail.com>
2026-07-13 02:13:45 +0700

    index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,37 @@
-console.log("Market Insight Indonesia");
-console.log("System Ready...");
+const https = require("https");
+
+function getPrice() {
+  return new Promise((resolve, reject) => {
+    https.get(
+      "https://api.binance.com/api/v3/ticker/price?symbol=PAXGUSDT",
+      (res) => {
+        let data = "";
+
+        res.on("data", chunk => {
+          data += chunk;
+        });
+
+        res.on("end", () => {
+          try {
+            const json = JSON.parse(data);
+            resolve(json.price);
+          } catch (err) {
+            reject(err);
+          }
+        });
+      }
+    ).on("error", reject);
+  });
+}
+
+async function main() {
+  console.log("=== Market Insight Indonesia ===");
+  console.log("System Running...");
+
+  const price = await getPrice();
+
+  console.log("XAUUSD Price:", price);
+  console.log("Data Engine OK");
+}
+
+main();
