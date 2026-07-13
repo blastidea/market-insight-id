b1261e716f051f6eaf99973c4410ff36bad24b0f
blastidea <blastidea@gmail.com>
2026-07-13 07:21:48 +0700

    Update market.js

--- src/market.js
+++ src/market.js
@@ -2,16 +2,26 @@ const axios = require("axios");
 const config = require("./config");
 
 async function getCandles() {
-  const url =
-    `https://api.twelvedata.com/time_series` +
-    `?symbol=${encodeURIComponent(config.symbol)}` +
-    `&interval=${config.interval}` +
-    `&outputsize=${config.outputSize}` +
-    `&apikey=${config.apiKey}`;
+  try {
+    const url =
+      `https://api.twelvedata.com/time_series` +
+      `?symbol=${encodeURIComponent(config.symbol)}` +
+      `&interval=${config.interval}` +
+      `&outputsize=${config.outputSize}` +
+      `&apikey=${config.apiKey}`;
 
-  const response = await axios.get(url);
+    const response = await axios.get(url);
 
-  return response.data;
+    return response.data;
+
+  } catch (err) {
+
+    if (err.response) {
+      return err.response.data;
+    }
+
+    throw err;
+  }
 }
 
 module.exports = {
