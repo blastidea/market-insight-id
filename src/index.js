5c39b46b10335fbd21ca129bcf8020ec028f6c85
blastidea <blastidea@gmail.com>
2026-07-13 15:47:39 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
+const {
+ getMultiTimeframe
+} = require("./src/timeframe");
+
+(async()=>{
+
+ const mtf = await getMultiTimeframe();
+
+ console.log("========== MTF TEST ==========");
+
+ console.log("H4 :", mtf.H4.interval, mtf.H4.status);
+ console.log("H1 :", mtf.H1.interval, mtf.H1.status);
+ console.log("M30:", mtf.M30.interval, mtf.M30.status);
+ console.log("M15:", mtf.M15.interval, mtf.M15.status);
+ console.log("M5 :", mtf.M5.interval, mtf.M5.status);
+
+})();
+
+
+
 const { getCandles } = require("./market");
 
 const {
