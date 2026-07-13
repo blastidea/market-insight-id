8fdf04f448c231be4f4669460deebc1d4fc3b7de
blastidea <blastidea@gmail.com>
2026-07-13 16:01:54 +0700

    Update market.js

--- src/market.js
+++ src/market.js
@@ -1,8 +1,3 @@
-// src/market.js
-// Market Data Engine v1.1
-// Support Multi Timeframe
-
-
 const axios = require("axios");
 const config = require("./config");
 
@@ -10,7 +5,6 @@ const config = require("./config");
 async function getCandles(interval = config.interval) {
 
   try {
-
 
     const url =
       `https://api.twelvedata.com/time_series` +
@@ -20,25 +14,17 @@ async function getCandles(interval = config.interval) {
       `&apikey=${config.apiKey}`;
 
 
-
     const response = await axios.get(url);
-
 
     const raw = response.data;
 
 
-
-    if (raw.status === "error") {
-
+    if(raw.status === "error"){
       return raw;
-
     }
 
 
-
     return {
-
-      status:"ready",
 
       symbol: raw.meta.symbol,
 
@@ -46,62 +32,26 @@ async function getCandles(interval = config.interval) {
 
       candles: raw.values.length,
 
+      latest: raw.values[0],
 
-      latest: {
-
-        datetime: raw.values[0].datetime,
-
-        open: Number(raw.values[0].open),
-
-        high: Number(raw.values[0].high),
-
-        low: Number(raw.values[0].low),
-
-        close: Number(raw.values[0].close)
-
-      },
-
-
-      history: raw.values.map(candle => ({
-
-        datetime:candle.datetime,
-
-        open:Number(candle.open),
-
-        high:Number(candle.high),
-
-        low:Number(candle.low),
-
-        close:Number(candle.close)
-
-      }))
-
+      history: raw.values
 
     };
 
 
-
   } catch(err){
 
-
     if(err.response){
-
       return err.response.data;
-
     }
 
-
     throw err;
-
 
   }
 
 }
 
 
-
 module.exports = {
-
   getCandles
-
 };
